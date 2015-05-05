af6a5fa343746368cbb8b1259185e26bbd91697c
CarinaAllmann <1410653052@W1-108-14.int.fh-kufstein.ac.at>
2015-05-05 12:36:02 +0200

    new commit

---
+++ UE_03_1410653052 Leistungsbeurteilung/src/Aufgabe3/Benennbar.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Aufgabe3;
+
+/**
+ *
+ * @author 1410653052
+ */
+public abstract class Benennbar 
+{
+   private String name;
+    
+    public abstract void setName(String n);
+    
+    public abstract String getName();
+    
+    
+    
+}
